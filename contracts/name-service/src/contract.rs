40961db622b0f3c9bc19da17df6cadabd1da2872
mattverse <mattpark1028@gmail.com>
2022-11-12 16:52:50 +0900

    Fix library build bug

--- contracts/name-service/src/contract.rs
+++ contracts/name-service/src/contract.rs
@@ -2,7 +2,7 @@ use cw2::set_contract_version;
 
 use cosmwasm_std::Order::Ascending;
 use cosmwasm_std::{
-    entry_point, to_binary, Addr, Binary, Deps, DepsMut, Env, MessageInfo, Response,
+     to_binary, Addr, Binary, Deps, DepsMut, Env, MessageInfo, Response,
     StdError, StdResult, 
 };
 
