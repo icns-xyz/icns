cf37303ad9554439de6e0dc94cf1939e846e2b91
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-11-18 15:41:31 +0700

    add more test case to set admin

--- contracts/name/src/tests/admin.rs
+++ contracts/name/src/tests/admin.rs
@@ -8,7 +8,7 @@ use crate::{
 
 use cosmwasm_std::Addr;
 use cw721_base::ContractError;
-use cw_multi_test::Executor;
+use cw_multi_test::{BasicApp, Executor};
 
 #[test]
 fn only_admin_can_set_new_admin() {
@@ -16,51 +16,47 @@ fn only_admin_can_set_new_admin() {
         mut app,
         admin,
         contract_addr,
+        registry,
         ..
     } = EnvBuilder::default().with_transferable(false).build();
+
+    let get_admin = |app: &BasicApp| {
+        let AdminResponse { admin } = app
+            .wrap()
+            .query_wasm_smart(contract_addr.clone(), &QueryMsg::Admin {})
+            .unwrap();
+
+        admin
+    };
+
+    let set_admin = |app: &mut BasicApp, sender: Addr, admin: String| {
+        app.execute_contract(
+            sender,
+            contract_addr.clone(),
+            &ExecuteMsg::ICNSName(ICNSNameExecuteMsg::SetAdmin { admin }),
+            &[],
+        )
+    };
 
     let new_admin = Addr::unchecked("new_admin");
 
     // set new admin by non admin should fail
-    let err = app
-        .execute_contract(
-            new_admin.clone(),
-            contract_addr.clone(),
-            &ExecuteMsg::ICNSName(ICNSNameExecuteMsg::SetAdmin {
-                admin: new_admin.to_string(),
-            }),
-            &[],
-        )
-        .unwrap_err();
-
+    let err = set_admin(&mut app, new_admin.clone(), new_admin.to_string()).unwrap_err();
     assert_eq!(
         err.downcast_ref::<ContractError>().unwrap(),
         &ContractError::Unauthorized {}
     );
+    assert_eq!(get_admin(&app), admin.to_string());
 
-    // admin should not be changed
-    let res: AdminResponse = app
-        .wrap()
-        .query_wasm_smart(contract_addr.clone(), &QueryMsg::Admin {})
-        .unwrap();
-
-    assert_eq!(res.admin, admin.to_string());
+    // set admin by registry should fail
+    let err = set_admin(&mut app, registry, new_admin.to_string()).unwrap_err();
+    assert_eq!(
+        err.downcast_ref::<ContractError>().unwrap(),
+        &ContractError::Unauthorized {}
+    );
+    assert_eq!(get_admin(&app), admin.to_string());
 
     // set new admin by admin should succeed
-    app.execute_contract(
-        admin,
-        contract_addr.clone(),
-        &ExecuteMsg::ICNSName(ICNSNameExecuteMsg::SetAdmin {
-            admin: new_admin.to_string(),
-        }),
-        &[],
-    )
-    .unwrap();
-
-    let res: AdminResponse = app
-        .wrap()
-        .query_wasm_smart(contract_addr, &QueryMsg::Admin {})
-        .unwrap();
-
-    assert_eq!(res.admin, new_admin.to_string());
+    set_admin(&mut app, admin, new_admin.to_string()).unwrap();
+    assert_eq!(get_admin(&app), new_admin.to_string());
 }
