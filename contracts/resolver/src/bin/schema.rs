3802ed4bbebdbb74b6de97db59bf0a666ab33c27
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-12-06 13:42:00 +0700

    setup schema gen

--- contracts/resolver/src/bin/schema.rs
+++ contracts/resolver/src/bin/schema.rs
@@ -1,12 +1,12 @@
 use cosmwasm_schema::write_api;
 
-use icns_name_nft::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use icns_resolver::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
         execute: ExecuteMsg,
         query: QueryMsg,
-        migrate: MigrateMsg,
+        migrate: MigrateMsg
     }
 }
