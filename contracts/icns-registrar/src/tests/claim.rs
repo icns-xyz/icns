ab78cf929e93d39efa9891d6b5139a1199b216fa
mattverse <mattpark1028@gmail.com>
2022-12-09 17:08:16 +0900

    tests: send funds when fee is not set

--- contracts/icns-registrar/src/tests/claim.rs
+++ contracts/icns-registrar/src/tests/claim.rs
@@ -525,6 +525,90 @@ fn claim_name_with_fee() {
     assert_eq!(owner(&app, bob_name.to_string()).unwrap(), bob);
 }
 
+
+#[test]
+fn claim_name_without_fee() {
+    let bob = Addr::unchecked("bobaddr");
+    let alice = Addr::unchecked("aliceaddr");
+    // setup contracts
+    let mut app = AppBuilder::default().build(|router, _, storage| {
+        router
+            .bank
+            .init_balance(
+                storage,
+                &bob,
+                vec![
+                    Coin::new(100_000_000_000, "uosmo"),
+                    Coin::new(100_000_000_000, "uion"),
+                ],
+            )
+            .unwrap();
+    });
+
+    let verify_all = |verifying_msg: &str, verifiers: Vec<SigningKey>| -> Vec<Verification> {
+        verifiers
+            .iter()
+            .map(|verifier| Verification {
+                public_key: verifier.to_binary(),
+                signature: verifier.sign(verifying_msg.as_bytes()).unwrap().to_binary(),
+            })
+            .collect()
+    };
+
+    let name_nft_code_id = app.store_code(name_nft_contract());
+    let registrar_code_id = app.store_code(registrar_contract());
+    let admins = vec!["admin1".to_string(), "admin2".to_string()];
+
+    // setup name nft contract
+    let (_name_nft_contract_addr, registrar_contract_addr) = default_contracts_setup(
+        &mut app,
+        name_nft_code_id,
+        registrar_code_id,
+        admins.clone(),
+        None
+    );
+
+    let bob_name = "bob";
+    let multitest_chain_id = "cosmos-testnet-14002";
+    let unique_twitter_id = "1234567890";
+    let verifying_msg = format!(
+        r#"{{"name":"{bob_name}","claimer":"{bob}","contract_address":"{registrar_contract_addr}","chain_id":"{multitest_chain_id}","unique_twitter_id":"{unique_twitter_id}"}}"#,
+    );
+
+    app.execute_contract(
+            bob.clone(),
+            registrar_contract_addr.clone(),
+            &ExecuteMsg::Claim {
+                name: bob_name.to_string(),
+                verifying_msg: verifying_msg.clone(),
+                verifications: verify_all(&verifying_msg, vec![verifier4(), verifier3()]),
+                referral: None,
+            },
+            &[],
+        )
+        .unwrap();
+
+    let alice_name = "alice";
+    let unique_twitter_id = "1234567891";
+    let verifying_msg = format!(
+        r#"{{"name":"{alice_name}","claimer":"{bob}","contract_address":"{registrar_contract_addr}","chain_id":"{multitest_chain_id}","unique_twitter_id":"{unique_twitter_id}"}}"#,
+    );
+
+    // try sending fund when fee is not set, it should not error
+    app.execute_contract(
+        bob.clone(),
+            registrar_contract_addr.clone(),
+            &ExecuteMsg::Claim {
+                name: alice_name.to_string(),
+                verifying_msg: verifying_msg.clone(),
+                verifications: verify_all(&verifying_msg, vec![verifier4(), verifier3()]),
+                referral: None,
+            },
+            &[Coin::new(100, "uion")],
+        )
+        .unwrap();
+}
+
 #[test]
 fn claim_name_with_referral() {
     // setup contracts
